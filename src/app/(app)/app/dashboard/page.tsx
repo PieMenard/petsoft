5fcbf7710ec95281197ab68c355b1334e1bd1a23
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-05-27 14:16:22 -0400

    style dashboard page

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+import Branding from '@/app/components/branding';
+import Stats from '@/app/components/stats';
+
 export default function DashBoardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <Branding />
+        <Stats />
+      </div>
+    </main>
+  );
 }
