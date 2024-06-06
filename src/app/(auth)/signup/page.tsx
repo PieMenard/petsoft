9e5849597813bc99dd212b21a9d0fa09fb0eb848
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-06 11:07:01 -0400

    style login and signup pages

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,18 @@
-export default function SignUpPge() {
-  return <div>Sign up page</div>;
+import AuthForm from '@/components/auth-form';
+import H1 from '@/components/h1';
+import Link from 'next/link';
+
+export default function SignUpPage() {
+  return (
+    <main>
+      <H1 className="mb-5 text-center">Sign Up</H1>
+      <AuthForm />
+      <p className="mt-6 text-sm text-zinc-500">
+        Already have an account?{' '}
+        <Link href="/login" className="font-medium">
+          Log in
+        </Link>
+      </p>
+    </main>
+  );
 }
