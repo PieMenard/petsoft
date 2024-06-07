bfb43f6a06eb4b31763f49e539401223bbbe7cef
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-07 14:32:39 -0400

    implement user login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,7 @@
+import bcrypt from 'bcryptjs';
 import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./db";
 
 const config = {
     pages: {
@@ -8,18 +11,48 @@ const config = {
     //     maxAge: 30 * 24 * 60 * 60,
     //     strategy: "jwt"
     // },
-    providers: [],
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                // runs on login
+                const { email, password } = credentials;
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email
+                    }
+                });
+                if (!user) {
+                    console.log("No user found");
+                    return null;
+                }
+                const passwordsMatch = await bcrypt.compare(password, user.hashedPassword);
+                if (!passwordsMatch) {
+                    console.log("Invalid credentials");
+                    return null;
+                }
+                return user;
+            },
+        }),
+    ],
     callbacks: {
-        authorized: ({ request }) => {
+        authorized: ({ auth, request }) => {
+            //runs on every request with middleware
+            const isLoggedIn = Boolean(auth?.user);
             const isTryingToAccessApp = request.nextUrl.pathname.includes('/app');
-            if (isTryingToAccessApp) {
+            if (!isLoggedIn && isTryingToAccessApp) {
                 return false;
             }
-            else {
+
+            if (isLoggedIn && isTryingToAccessApp) {
+                return true;
+            }
+
+            if (!isTryingToAccessApp) {
                 return true;
             }
         }
     }
 } satisfies NextAuthConfig;
 
-export const { auth } = NextAuth(config);
+export const { auth, signIn } = NextAuth(config);
