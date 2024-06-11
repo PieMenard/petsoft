d597a51308222191e055ec8637781a0a49bc45f3
PieMenard <pinedo.m.gustavo@gmail.com>
2024-06-10 23:38:33 -0400

    implement auth error nad loading handling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginUpPage() {
   return (
     <main>
       <H1 className="mb-5 text-center">Log In</H1>
-      <AuthForm type="login" />
+      <AuthForm type="logIn" />
       <p className="mt-6 text-sm text-zinc-500">
         No account yet?{' '}
         <Link href="/signup" className="font-medium">
--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -1,15 +1,21 @@
+'use client';
+
 import { logIn, signUp } from '@/actions/actions';
-import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
+import AuthFormBtn from './auth-form-btn';
+import { useFormState } from 'react-dom';
 
 type AuthFormProps = {
-  type: 'login' | 'signup';
+  type: 'logIn' | 'signUp';
 };
 
 export default function AuthForm({ type }: AuthFormProps) {
+  const [signUpError, dispatchSignUp] = useFormState(signUp, undefined);
+  const [logInError, dispatchLogIn] = useFormState(logIn, undefined);
+
   return (
-    <form action={type === 'login' ? logIn : signUp}>
+    <form action={type === 'logIn' ? dispatchLogIn : dispatchSignUp}>
       <div className="space-y-1">
         <Label htmlFor="email">Email</Label>
         <Input id="email" name="email" type="email" required maxLength={100} />
@@ -26,7 +32,14 @@ export default function AuthForm({ type }: AuthFormProps) {
         />
       </div>
 
-      <Button>{type === 'login' ? 'Log In' : 'Sign Up'}</Button>
+      <AuthFormBtn type={type} />
+
+      {signUpError && (
+        <p className="text-red-500 text-sm mt-2">{signUpError.message}</p>
+      )}
+      {logInError && (
+        <p className="text-red-500 text-sm mt-2">{logInError.message}</p>
+      )}
     </form>
   );
 }
