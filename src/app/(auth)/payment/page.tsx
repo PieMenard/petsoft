8352affef508dc0d5c93cb3cdffaab072d2029a4
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-06-11 13:18:23 -0400

    add loading state to checkout button

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,18 +3,39 @@
 import { createCheckoutSession } from '@/actions/actions';
 import H1 from '@/components/h1';
 import { Button } from '@/components/ui/button';
+import { startTransition, useTransition } from 'react';
 
-export default function PaymentPage() {
+export default function PaymentPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const [isPending, startTransition] = useTransition();
+
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>PetSoft access requires payment</H1>
-      <Button
-        onClick={() => {
-          createCheckoutSession();
-        }}
-      >
-        Buy lifetime access for $199
-      </Button>
+      {!searchParams.success && (
+        <Button
+          disabled={isPending}
+          onClick={async () => {
+            startTransition(async () => {
+              await createCheckoutSession();
+            });
+          }}
+        >
+          Buy lifetime access for $199
+        </Button>
+      )}
+
+      {searchParams.success && (
+        <p className="text-sm text-green-700">
+          Payment successful! You now have lifetime access to PetSoft.
+        </p>
+      )}
+      {searchParams.cancelled && (
+        <p className="text-sm text-green-700">Payment cancelled. Try again.</p>
+      )}
     </main>
   );
 }
