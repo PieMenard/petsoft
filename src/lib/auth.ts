fd97a0aa44e1009a2a1e7718fd8326aafe5e59f8
PieMenard <pinedo.m.gustavo@gmail.com>
2024-06-10 21:19:44 -0400

    fix type PetEssentiials

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,83 +1,91 @@
-import { TAuth, authSchema } from './validations';
+import { authSchema } from './validations';
 import bcrypt from 'bcryptjs';
-import NextAuth, { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
+import NextAuth, { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 import { getUserByEmail } from './server-utils';
 
 const config = {
-    pages: {
-        signIn: "/login",
-    },
-    // session: {
-    //     maxAge: 30 * 24 * 60 * 60,
-    //     strategy: "jwt"
-    // },
-    providers: [
-        Credentials({
-            async authorize(credentials) {
-                //runs on login
+  pages: {
+    signIn: '/login',
+  },
+  // session: {
+  //     maxAge: 30 * 24 * 60 * 60,
+  //     strategy: "jwt"
+  // },
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        //runs on login
 
-                //validation
-                const validatedFormData = authSchema.safeParse(credentials);
-                if (!validatedFormData.success) {
-                    return null
-                }
-
-                // extract values
-                const { email, password } = validatedFormData.data;
-
-                const user = await getUserByEmail(email);
-                if (!user) {
-                    console.log("No user found");
-                    return null;
-                }
-                const passwordsMatch = await bcrypt.compare(password, user.hashedPassword);
-                if (!passwordsMatch) {
-                    console.log("Invalid credentials");
-                    return null;
-                }
-                return user;
-            },
-        }),
-    ],
-    callbacks: {
-        authorized: ({ auth, request }) => {
-            //runs on every request with middleware
-            const isLoggedIn = Boolean(auth?.user);
-            const isTryingToAccessApp = request.nextUrl.pathname.includes('/app');
-            if (!isLoggedIn && isTryingToAccessApp) {
-                return false;
-            }
-
-            if (isLoggedIn && isTryingToAccessApp) {
-                return true;
-            }
-
-            if (isLoggedIn && !isTryingToAccessApp) {
-                return Response.redirect(new URL("/app/dashboard", request.nextUrl));
-            }
-
-            if (!isLoggedIn && !isTryingToAccessApp) {
-                return true;
-            }
-
-            return false;
-        },
-        jwt: ({ token, user }) => {
-            if (user) {
-                //on sign in
-                token.userId = user.id;
-            }
-            return token;
-        },
-        session: ({ session, token }) => {
-            if (session.user) {
-                session.user.id = token.userId
-            }
-
-            return session;
+        //validation
+        const validatedFormData = authSchema.safeParse(credentials);
+        if (!validatedFormData.success) {
+          return null;
         }
+
+        // extract values
+        const { email, password } = validatedFormData.data;
+
+        const user = await getUserByEmail(email);
+        if (!user) {
+          console.log('No user found');
+          return null;
+        }
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+        if (!passwordsMatch) {
+          console.log('Invalid credentials');
+          return null;
+        }
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    authorized: ({ auth, request }) => {
+      //runs on every request with middleware
+      const isLoggedIn = Boolean(auth?.user);
+      const isTryingToAccessApp = request.nextUrl.pathname.includes('/app');
+      if (!isLoggedIn && isTryingToAccessApp) {
+        return false;
+      }
+
+      if (isLoggedIn && isTryingToAccessApp) {
+        return true;
+      }
+
+      if (isLoggedIn && !isTryingToAccessApp) {
+        return Response.redirect(new URL('/app/dashboard', request.nextUrl));
+      }
+
+      if (!isLoggedIn && !isTryingToAccessApp) {
+        return true;
+      }
+
+      return false;
     },
+    jwt: ({ token, user }) => {
+      if (user) {
+        //on sign in
+        token.userId = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (session.user) {
+        session.user.id = token.userId;
+      }
+
+      return session;
+    },
+  },
 } satisfies NextAuthConfig;
 
-export const { auth, signIn, signOut, handlers: { GET, POST } } = NextAuth(config);
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth(config);
